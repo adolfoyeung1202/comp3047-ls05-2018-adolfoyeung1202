7f94d530705c13d531cd91b4059ad8d226442eac
16222407 <16222407@cs12216.comp.hkbu.edu.hk>
2018-10-24 18:23:10 +0800

    Initial commit

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -27,6 +27,23 @@ module.exports.bootstrap = async function (done) {
   // ]);
   // ```
 
+  sails.getInvalidIdMsg = function (opts) {
+
+    if (opts.id != undefined && isNaN(parseInt(opts.id))) {
+      return "Primary key specfied is invalid (incorrect type).";
+    }
+
+    if (opts.fk != undefined && isNaN(parseInt(opts.fk))) {
+      return "Foreign key specfied is invalid (incorrect type).";
+    }
+
+    return null;        // falsy
+
+  }
+
+  sails.bcrypt = require('bcrypt');
+  const saltRounds = 10;
+
   if (await Person.count() > 0) {
     return done();
   }
@@ -36,6 +53,28 @@ module.exports.bootstrap = async function (done) {
     { name: "Kenny Cheng", age: 22 }
     // etc.
   ]);
+
+  const hash = await sails.bcrypt.hash('123456', saltRounds);
+
+  await User.createEach([
+    { "username": "admin", "password": hash },
+    { "username": "boss", "password": hash }
+    // etc.
+  ]);
+
+  const martin = await Person.findOne({ name: "Martin Choy" });
+  const kenny = await Person.findOne({ name: "Kenny Cheng" });
+  const admin = await User.findOne({ username: "admin" });
+  const boss = await User.findOne({ username: "boss" });
+
+  await User.addToCollection(admin.id, 'supervises').members(kenny.id);
+  await User.addToCollection(boss.id, 'supervises').members([martin.id, kenny.id]);
+
+  //  await User.createEach([
+  //    { "username": "admin", "password": "123456" },
+  //    { "username": "boss", "password": "123456" }
+  // etc.
+  //  ]);
 
   // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
   // (otherwise your server will never lift, since it's waiting on the bootstrap)
